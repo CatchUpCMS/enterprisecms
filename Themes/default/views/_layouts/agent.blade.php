85e297e74c75fb83e650b93e6165bc40f2f7106f
Niels Drost <nielsdrost7@gmail.com>
2017-03-26 19:50:59 +0200

    Quick commit

--- Themes/default/views/_layouts/agent.blade.php
+++ Themes/default/views/_layouts/agent.blade.php
@@ -35,10 +35,9 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('themes/' . $activeTheme . '/assets/css/reset.css') }}">
 
-
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('themes/' . $activeTheme . '/assets/admin_lte/dist/css/AdminLTE.css') }}">
     <style type="text/css">
 
         body {
@@ -61,6 +60,16 @@
             -moz-transition: all 0.5s ease;
             -o-transition: all 0.5s ease;
             transition: all 0.5s ease;
+        }
+
+        .settingdivblue {
+            width: 70px;
+            height: 70px;
+            margin: 0 auto;
+            text-align: center;
+            border: 5px solid #C4D8E4;
+            border-radius: 100%;
+            padding-top: 5px;
         }
 
         .sidebar {
@@ -287,190 +296,18 @@
     </aside>
 
     <section class="main">
+        @if (count($errors) > 0)
+            @include($activeTheme . '::' . '_partials.errors')
+        @endif
 
-        <section class="tab-content">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
 
-            <section class="tab-pane active fade in content" id="dashboard">
-
-                <div class="row">
-
-                    <div class="col-xs-6 col-sm-3">
-                        <div class="panel panel-primary">
-                            <div class="panel-body">
-                                <br/><br/><br/><br/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-6 col-sm-3">
-                        <div class="panel panel-success">
-                            <div class="panel-body">
-                                <br/><br/><br/><br/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-6 col-sm-3">
-                        <div class="panel panel-danger">
-                            <div class="panel-body">
-                                <br/><br/><br/><br/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-6 col-sm-3">
-                        <div class="panel panel-warning">
-                            <div class="panel-body">
-                                <br/><br/><br/><br/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-9">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                Something
-                            </div>
-                            <div class="panel-body">
-                                This layout uses tabs to demonstrate what you could do with it. It probably makes more
-                                sense to use individual pages/templates in a production app.
-                                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-3">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                Something
-                            </div>
-                            <div class="panel-body">
-                                The sidebar is naturally responsive according to screen width. You can toggle it using
-                                the menu button in the topbar. Test it out by increasing/decreasing your screen width
-                                and watch it respond.
-                            </div>
-                        </div>
-
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                Kaitani Labs
-                            </div>
-                            <div class="panel-body">
-                                Designed by <a href="http://www.kaitanilabs.com" target="_blank">Kaitani Labs</a></a>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-            </section>
-
-            <section class="tab-pane fade" id="configuration">
-                <nav class="subbar">
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#access" data-toggle="tab"><i class="fa fa-code"></i>
-                                <span>System</span></a></li>
-                        <li><a href="#roles" data-toggle="tab"><i class="fa fa-user"></i> <span>Roles</span></a></li>
-                    </ul>
-                </nav>
-
-                <section class="tab-content content">
-
-                    <section class="tab-pane active fade in" id="access">
-
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/><br/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-xs-12 col-sm-4">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/><br/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-xs-12 col-sm-4">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/><br/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-xs-12 col-sm-4">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/><br/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </section>
-
-                    <section class="tab-pane fade" id="roles">
-
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-8 col-md-9">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="hidden-xs col-sm-4 col-md-3">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/>
-                                    </div>
-                                </div>
-
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        Something
-                                    </div>
-                                    <div class="panel-body">
-                                        <br/><br/><br/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
-                </section>
-            </section>
-            <section class="tab-pane fade" id="users">
-            </section
-            <section class="tab-pane fade" id="mail">
-            </section>
-        </section>
-    </section>
-
+        @yield('content')
+    </section><!-- -->
 </article>
 
 
--- Themes/flatly/views/_partials/admin/content.blade.php
+++ Themes/flatly/views/_partials/admin/content.blade.php
@@ -1,13 +1,13 @@
 @include('flash::message')
 
 @if (count($errors) > 0)
-	@include($activeTheme . '::' . '_partials.errors')
+    @include($activeTheme . '::' . '_partials.errors')
 @endif
 
 @if (session('status'))
-	<div class="alert alert-success">
-		{{ session('status') }}
-	</div>
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
 @endif
 
 @yield('content')
